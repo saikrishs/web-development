e8430405a8b2249c8774287d39cfe0e308a80757
saikrishs <sai.srungavarapu@gmail.com>
2024-02-07 08:48:21 -0500

    Updated Deployment Script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,8 +4,8 @@ set -e
 echo "Deployment started ..."
 
 # Pull the latest version of the app
-git fetch origin master
-git reset --hard origin/master
-git pull origin master
+git fetch origin main
+git reset --hard origin/main
+git pull origin main
 
 echo "Deployment finished!"
